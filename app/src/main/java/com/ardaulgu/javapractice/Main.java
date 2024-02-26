bfcc299dda4ef0be842262ded09f220e4862e1ea
Arda ULGU <arda.ulgu.35@gmail.com>
2024-02-26 16:24:01 +0300

    JavaPractice

---
+++ app/src/main/java/com/ardaulgu/javapractice/Main.java
@@ -0,0 +1,10 @@
+package com.ardaulgu.javapractice;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println(5*2);
+    }
+
+}
