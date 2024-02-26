a9c033fa344c1192d5aa1aaab8fa908ab3d77419
Arda ULGU <arda.ulgu.35@gmail.com>
2024-02-26 16:35:33 +0300

    JavaPractice 1

--- app/src/main/java/com/ardaulgu/javapractice/Main.java
+++ app/src/main/java/com/ardaulgu/javapractice/Main.java
@@ -3,8 +3,32 @@ package com.ardaulgu.javapractice;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println(5*2);
+
+        //Variables
+
+        //Integer - Long
+
+        int age = 20;
+        System.out.println(10*age);
+        System.out.println(age/5);
+
+        int x = 5;
+        int y = 10;
+        System.out.println(y/x);
+
+        long myLong = 10;
+        System.out.println(myLong/5);
+
+        //Double - Float
+
+        double z = 5.0;
+        double a = 11.0;
+        System.out.println(a/z);
+
+        float myFloat = 10.0f;
+
+
+
     }
 
 }
